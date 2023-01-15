5753a3b2382bb7a043ad2c6b03d731dbd3695cc4
SergioS2001 <39836@ufp.edu.pt>
2023-01-15 09:51:04 +0000

    master

--- resources/views/admin/index.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.admin') <!-- VERIFICAR SE OS NAMES DOS CONTAINERS ESTÃO CORRETOS -->
-
-@section('container-fluid')
-<div class="card">
-    <div class="card-body">
-        <h1> Joana's code </h1>
-    </div>
-</div>
